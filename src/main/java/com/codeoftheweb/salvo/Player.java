7980b808621a3f3a7a9db1fc5193c0e1f86dd32b
YuriSolomon <juristein21@gmail.com>
2018-10-10 12:34:29 +0200

    first commit

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,35 @@
+package com.codeoftheweb.salvo;
+
+public class Player {
+
+    private String userName;
+    private String email;
+
+    public Player() {};
+
+    public Player (String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return userName;
+    }
+}
