8ccf372956f57100de92ef3324a2522d2b5dd103
YuriSolomon <juristein21@gmail.com>
2018-10-18 16:57:03 +0200

    get grid

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -17,6 +17,38 @@ function getData() {
                 console.log(this.data);
 
             })
+    },
+    created() {
+        this.buildTable();
+    },
+    methods: {
+        buildTable() {
+            let table = document.getElementById("grid");
+            let tHead = document.createElement("thead");
+            let tBody = document.createElement("tbody");
+            
+            let tem1 = '';
+            let tem2 = '';
+            let header = [" ",1,2,3,4,5,6,7,8,9,10];
+            let body = ["","A","B","C","D","E","F","G","H","I","J"]
+            for (let i=0; i<header.length; i++){
+                tem1 += `<td>${header[i]}</td>`
+                if (i>0) {
+                    tem2 += `<tr><td>${body[i]}</td>`
+                    for (let j = 1; j < header.length; j++) {
+                    tem2 += `<td></td>`    
+                    }
+                    tem2 += `</tr>`
+                }
+            }
+
+            tHead.innerHTML = tem1;
+            tBody.innerHTML = tem2;
+            
+            table.append(tHead, tBody);
+           
+
+        }
     }
   })
 };
